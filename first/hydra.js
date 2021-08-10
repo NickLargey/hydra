613f103052fe6728fc0e39262cc90abe9133cad5
Nick.Largey@gmail.com <Nick.Largey@gmail.com>
2021-08-10 18:01:13 -0400

    fun with bitshift

--- first/hydra.js
+++ first/hydra.js
@@ -1,7 +1,18 @@
-var hydra = new Hydra({
-    canvas: document.getElementById("c"),
-    detectAudio: false,
-    enableStreamCapture: false,
-  });
+const canvas = document.getElementById("c");
 
-  osc(10, 0.1, 0.8).rotate(0, 0.1).kaleid().color(-1, 1).out();
+// canvas.width = 1024;
+// canvas.height = 1024;
+
+var hydra = new Hydra({
+  canvas,
+  detectAudio: false,
+  enableStreamCapture: false,
+});
+
+osc(10, 0.1, 0.8)
+  .rotate(0, 1)
+  .pixelate(16, 16)
+  .color(-4, 4)
+  .invert(2)
+  .modulate(noise(5 << 1), 2 >> 1)
+  .out();
