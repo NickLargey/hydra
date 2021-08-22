02e8350e02199de5bcfccc519d338078ce27da5c
Nick.Largey@gmail.com <Nick.Largey@gmail.com>
2021-08-22 12:17:43 -0400

    second hydra head WIP

--- second/hydra.js
+++ second/hydra.js
@@ -9,5 +9,11 @@ var hydra = new Hydra({
 });
 
 var button = () => s0.initCam();
-src(s0).kaleid(2).out(o2);
+src(s0)
+  .rotate(({ time }) => (time % 360) / 8)
+  .modulatePixelate(noise(25, 0.5), 75)
+  .add(noise(0.01))
+  .color(1, 0.01, 0.2)
+  .mask(gradient(0.125).pixelate(16))
+  .out(o2);
 render(o2);
